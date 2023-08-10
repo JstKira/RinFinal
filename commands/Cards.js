cc1a05b3a9d3fe0b7da80ccfd2bf39c93ac47618
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-10 06:11:35 +0300

    Update Cards.js

--- commands/Cards.js
+++ commands/Cards.js
@@ -1,1 +1,34 @@
+const axios = require('axios')
+const { sck1, tiny, fancytext, listall,cmd,ffmpeg } = require('../lib/')
+const fs = require('fs-extra');
+const { exec } = require('child_process')
+const Card = require('./lib/database/Card.js');
+
+cmd({
+  pattern: 'ارسل-بطاقة',
+  desc: 'يرسل بطاقة عشوائية',
+  category: 'بطاقات',
+  owner: true,
+  filename: __filename,
+}, async (message, match, citel, text, { isCreator }) => {
+  // Check if the user is the owner of the bot
+  if (!isCreator) {
+    citel.reply(tlang().owner);
+    return;
+  }
+
+  // Get a random card from the database
+  const randomCard = await Card.findOne().skip(Math.floor(Math.random() * await Card.countDocuments()));
+
+  // Check if a card is found
+  if (!randomCard) {
+    citel.reply('لا توجد بطاقات متاحة في الوقت الحالي');
+    return;
+  }
+
+  // Send the card details as a reply
+  await message.replyWithPhoto({ source: randomCard.photo }, {
+    caption: `اسم البطاقة: ${randomCard.name}\nTier: ${randomCard.tier}\nPrice: ${randomCard.price}`,
+  });
+});
 

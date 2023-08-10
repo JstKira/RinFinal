c09c1da280c283d537279cc946bea8a30231c36c
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-10 07:36:20 +0300

    Update Cards.js

--- commands/Cards.js
+++ commands/Cards.js
@@ -69,11 +69,14 @@ cmd({
   owner: true,
   filename: __filename,
   usage: 'أضف-بطاقة <اسم البطاقة> <تصنيف> <رابط الصورة>',
-}, async (message, match, citel, text, { isCreator }) => {
+}, async (match, citel, text, { isCreator }) => {
+  // Check if the user is the owner of the bot
   if (!isCreator) {
-    citel.reply('خاص بغومونريونغ');
+    citel.reply(tlang().owner);
     return;
   }
+
+  
 
   // Parse the command arguments
   const [command, cardName, tier, photoLink] = text.split(' ');

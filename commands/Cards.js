98665faa694c84f8492d42724d2ac2e86093b74e
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-10 06:21:29 +0300

    Update Cards.js

--- commands/Cards.js
+++ commands/Cards.js
@@ -1,16 +1,17 @@
-const axios = require('axios')
+const axios = require('axios');
 const fs = require('fs-extra');
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 const Card = require('./lib/database/Card.js');
- const { tlang, getAdmin, prefix, Config, sck, fetchJson, runtime,cmd,getBuffer } = require('../lib')
- let { dBinary, eBinary } = require("../lib/binary");
+const { tlang, getAdmin, prefix, Config, sck, fetchJson, runtime, cmd, getBuffer } = require('../lib');
+let { dBinary, eBinary } = require('../lib/binary');
+
 cmd({
   pattern: 'ارسل-بطاقة',
   desc: 'يرسل بطاقة عشوائية',
   category: 'بطاقات',
   owner: true,
   filename: __filename,
-}, async (message, match, citel, text, { isCreator }) => {
+}, async (message, match, citel, text, { isCreator, Void }) => {
   // Check if the user is the owner of the bot
   if (!isCreator) {
     citel.reply(tlang().owner);
@@ -26,9 +27,34 @@ cmd({
     return;
   }
 
-  // Send the card details as a reply
-  await message.replyWithPhoto({ source: randomCard.photo }, {
-    caption: `اسم البطاقة: ${randomCard.name}\nTier: ${randomCard.tier}\nPrice: ${randomCard.price}`,
-  });
-});
+  // Download the photo
+  const photoResponse = await axios.get(randomCard.photo, { responseType: 'arraybuffer' });
+  const photoBuffer = Buffer.from(photoResponse.data, 'binary');
 
+  // Save the photo locally
+  const photoPath = './temp/photo.png';
+  await fs.writeFile(photoPath, photoBuffer);
+
+  // Send the photo as a reply with card details
+  if (mime === 'imageMessage' || mime === 'stickerMessage') {
+    let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
+    let name = await getRandom('.png');
+    exec(`ffmpeg -i ${media} ${name}`, async (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let buffer = fs.readFileSync(name);
+      await Void.sendMessage(citel.chat, { image: buffer }, { quoted: citel });
+
+      // Cleanup temporary files
+      fs.unlinkSync(media);
+      fs.unlinkSync(name);
+    });
+  } else {
+    await citel.reply({ file: photoPath }, `اسم البطاقة: ${randomCard.name}\nTier: ${randomCard.tier}\nPrice: ${randomCard.price}`);
+  }
+
+  // Remove the temporary photo file
+  await fs.unlink(photoPath);
+});

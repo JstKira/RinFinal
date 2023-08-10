ba8972357d3b7a129dcd0b721bf0bb2bcf5a3e24
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-10 08:05:01 +0300

    Update Cards.js

--- commands/Cards.js
+++ commands/Cards.js
@@ -9,57 +9,42 @@ cmd({
   pattern: 'ارسل-بطاقة',
   desc: 'يرسل بطاقة عشوائية',
   category: 'بطاقات',
-  owner: true,
   filename: __filename,
-}, async (match, citel, text, { isCreator }) => {
-  // Check if the user is the owner of the bot
-  if (!isCreator) {
-    citel.reply(tlang().owner);
-    return;
+}, async (match, citel) => {
+  try {
+    // Get the count of cards in the database
+    const cardCount = await Card.countDocuments();
+
+    // Generate a random index within the range of card count
+    const randomIndex = Math.floor(Math.random() * cardCount);
+
+    // Find a random card document using the random index
+    const randomCard = await Card.findOne().skip(randomIndex);
+
+    // If no card is found, handle the error
+    if (!randomCard) {
+      citel.reply('لا توجد بطاقات متاحة في الوقت الحالي.');
+      return;
+    }
+
+    // Download the photo
+    const photoResponse = await axios.get(randomCard.photo, { responseType: 'arraybuffer' });
+    const photoBuffer = Buffer.from(photoResponse.data, 'binary');
+
+    // Save the photo locally
+    const photoPath = './temp/photo.png';
+    await fs.writeFile(photoPath, photoBuffer);
+
+    // Send the photo as a reply with card details
+    await citel.reply({ file: photoPath }, `اسم البطاقة: ${randomCard.name}\nتصنيف: ${randomCard.tier}`);
+
+    // Remove the temporary photo file
+    await fs.unlink(photoPath);
+  } catch (error) {
+    console.error(error);
+    citel.reply('حدث خطأ أثناء استرجاع البطاقة العشوائية.');
   }
-
-
-  // Get a random card from the database
-  const randomCard = await Card.findOne().skip(Math.floor(Math.random() * await Card.countDocuments()));
-
-  // Check if a card is found
-  if (!randomCard) {
-    citel.reply('لا توجد بطاقات متاحة في الوقت الحالي');
-    return;
-  }
-
-  // Download the photo
-  const photoResponse = await axios.get(randomCard.photo, { responseType: 'arraybuffer' });
-  const photoBuffer = Buffer.from(photoResponse.data, 'binary');
-
-  // Save the photo locally
-  const photoPath = './temp/photo.png';
-  await fs.writeFile(photoPath, photoBuffer);
-
-  // Send the photo as a reply with card details
-  if (mime === 'imageMessage' || mime === 'stickerMessage') {
-    let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
-    let name = await getRandom('.png');
-    exec(`ffmpeg -i ${media} ${name}`, async (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      let buffer = fs.readFileSync(name);
-      await Void.sendMessage(citel.chat, { image: buffer }, { quoted: citel });
-
-      // Cleanup temporary files
-      fs.unlinkSync(media);
-      fs.unlinkSync(name);
-    });
-  } else {
-    await citel.reply({ file: photoPath }, `اسم البطاقة: ${randomCard.name}\nTier: ${randomCard.tier}\nPrice: ${randomCard.price}`);
-  }
-
-  // Remove the temporary photo file
-  await fs.unlink(photoPath);
 });
-
 
 
 

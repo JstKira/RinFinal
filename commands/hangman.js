c5c6adf2777bd9b584b1af8d01ac5346b8ab38cc
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-17 00:58:58 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,43 +1,38 @@
-const Hangman = require('../lib/hangman.js');
-const words = require('../lib/words');
-
 const { cmd } = require("../lib/");
-const prefixRegex = /^!/; // Update the prefixRegex according to your command prefix
 
-cmd.command = async (Void, citel) => {
-  const sender = citel.sender;
-  const reply = citel.reply;
+cmd({
+  pattern: 'hangman',
+  desc: 'Start a game of hangman',
+  category: 'Games',
+  command: async (Void, citel) => {
+    const sender = citel.sender;
+    const reply = citel.reply;
 
-  const randomWord = getRandomWord();
-  const hangman = new Hangman(randomWord);
+    const Hangman = require('../lib/hangman');
+    const words = require('../lib/words');
 
-  reply('Hangman Game Started!');
-  reply('Guess the word by entering one letter at a time.');
+    const randomWord = getRandomWord();
+    const hangman = new Hangman(randomWord);
 
-  hangman.printWordState();
+    reply('Hangman Game Started!');
+    reply('Guess the word by entering one letter at a time.');
 
-  const input = typeof citel.text === "string" ? citel.text.trim() : false;
+    hangman.printWordState();
 
-  if (input && input[1] && input[1] === " ") {
-    input = input[0] + input.slice(2);
-  }
+    const input = typeof citel.text === "string" ? citel.text.trim() : false;
 
-  const isCommand = input ? prefixRegex.test(input[0]) : false;
+    if (input && input[1] && input[1] === " ") {
+      input = input[0] + input.slice(2);
+    }
 
-  if (isCommand) {
-    const letter = input.slice(1).trim();
+    if (input.length === 1) {
+      const letter = input.trim();
 
-    hangman.makeGuess(letter);
+      hangman.makeGuess(letter);
 
-    if (hangman.maxGuesses === 0 || hangman.word.split('').every(char => hangman.guesses.has(char))) {
-      // Game over, handle the end of the game
+      if (hangman.maxGuesses === 0 || hangman.word.split('').every(char => hangman.guesses.has(char))) {
+        // Game over, handle the end of the game
+      }
     }
   }
-};
-
-function getRandomWord() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  return words[randomIndex];
-}
-
-module.exports = cmd;
+});

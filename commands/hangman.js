9a4074e099390e44181ef5b878f7fb0ca0782bf7
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-17 00:14:09 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,106 +1,59 @@
 const { cmd } = require("../lib/");
-const mongoose = require("mongoose"); // Require the 'mongoose' package
-const HangmanRoom = require("../lib/database/HangmanRoom.js"); // Update with the correct path to the HangmanRoom schema file
-const words = require('../lib/words.js'); // array of words to pick from
-
-mongoose.connect('mongodb+srv://thamerrin:Thamer12@rin.kikwxum.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB:', err);
-});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const words = require('../lib/words.js');
 
 cmd({
   pattern: 'hangman',
   desc: 'Start a game of hangman',
   category: 'Games'
 }, async (Void, citel) => {
+  const chosenWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+  let guessedLetters = [];
+  let remainingGuesses = 6;
+  let revealedWord = '';
 
-  const roomId = 'hangman-' + Date.now();
-  const word = randomWord(words); 
-  const maskedWord = maskWord(word);
-  const remainingGuesses = 6;
+  // Initialize the revealed word with dashes
+  for (let i = 0; i < chosenWord.length; i++) {
+    revealedWord += '-';
+  }
 
-  console.log(`Starting hangman in room ${roomId}`);
-  console.log(`Word: ${word}`);
-  console.log(`Masked Word: ${maskedWord}`);
+  citel.reply('Welcome to Hangman! Guess the word by typing a letter.');
 
-  citel.reply(`Starting hangman in room ${roomId}`);
-  citel.reply(maskedWord);
+  while (remainingGuesses > 0 && revealedWord.includes('-')) {
+    citel.reply(`\n${revealedWord}\nRemaining guesses: ${remainingGuesses}`);
 
-const room = new HangmanRoom({
-  roomId,
-  word,
-  maskedWord,
-  remainingGuesses
-});
-  await room.save(); // Save the game room data to MongoDB
+    const response = await citel.sender.nextMessage();
+    const guessedLetter = response.body.toLowerCase();
 
-});
-
-cmd({
-  on: 'text',
-  priority: 1
-}, async (Void, citel, text) => {
-
-  console.log(`Received text message: ${text}`);
-
-  const room = await HangmanRoom.findOne({
-    roomId: { $regex: /^hangman-/ }
-  });
-
-  if (!room || typeof text !== 'string' || text.length === 0) return;
-
-  const guess = text.toLowerCase()[0];
-
-  console.log(`Guess: ${guess}`);
-
-  if (room.word.includes(guess)) {
-    room.maskedWord = fillInLetter(room.maskedWord, room.word, guess);
-    if (!room.maskedWord.includes('_')) {
-      citel.reply(`You guessed the word: ${room.word}`);
-      await room.remove(); // Remove the game room data from MongoDB
-      return;
+    if (guessedLetters.includes(guessedLetter)) {
+      citel.reply(`You already guessed the letter "${guessedLetter}". Try again.`);
+      continue;
     }
+
+    guessedLetters.push(guessedLetter);
+
+    if (chosenWord.includes(guessedLetter)) {
+      // Update the revealed word with the correctly guessed letter
+      let tempWord = '';
+      for (let i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === guessedLetter) {
+          tempWord += guessedLetter;
+        } else {
+          tempWord += revealedWord[i];
+        }
+      }
+      revealedWord = tempWord;
+
+      citel.reply(`Good guess! The letter "${guessedLetter}" is in the word.`);
+    } else {
+      remainingGuesses--;
+
+      citel.reply(`Oops! The letter "${guessedLetter}" is not in the word. Remaining guesses: ${remainingGuesses}`);
+    }
+  }
+
+  if (remainingGuesses > 0) {
+    citel.reply(`Congratulations! You guessed the word "${chosenWord}" correctly.`);
   } else {
-    room.remainingGuesses--;
+    citel.reply(`Game over! The word was "${chosenWord}". Better luck next time.`);
   }
-
-  if (room.remainingGuesses === 0) {
-    citel.reply(`You lost! The word was ${room.word}`);
-    await room.remove(); // Remove the game room data from MongoDB
-    return;
-  }
-
-  console.log(`Remaining Guesses: ${room.remainingGuesses}`);
-  console.log(`Masked Word: ${room.maskedWord}`);
-
-  citel.reply(`Guess: ${guess}, Remaining Guesses: ${room.remainingGuesses}`);
-  citel.reply(room.maskedWord);
-
 });
-
-// Helper functions
-
-function randomWord(words) {
-  return words[Math.floor(Math.random() * words.length)];
-}
-
-function maskWord(word) {
-  return word.replace(/./g, '_');
-} 
-
-function fillInLetter(masked, word, letter) {
-  let idx = word.indexOf(letter);
-  while (idx !== -1) {
-    masked = masked.substring(0, idx) + letter + masked.substring(idx + 1);
-    idx = word.indexOf(letter, idx + 1);
-  }
-  return masked;
-}

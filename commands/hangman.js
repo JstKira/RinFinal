6f15e98ba7104d9a872ef999a6c001f0d0c03fad
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-16 21:42:44 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -2,86 +2,83 @@ const { cmd, parseJid, getAdmin, tlang } = require("../lib/");
 
 
 //----------------------
-cmd(
-  {
-    pattern: "hangman",
-    desc: "Play Hangman game",
-    filename: __filename,
-    category: "Games",
-  },
-  async (Void, citel, text) => {
-    if (!citel.isGroup) return citel.reply("This game can only be played in groups.");
-    let { prefix } = require('../lib');
-    
-    // Create Hangman game room
-    this.hangmanGame = this.hangmanGame ? this.hangmanGame : {};
-    let room = {
-      id: "hangman-" + +new Date(),
-      players: [citel.sender],
-      word: "apple", // You can replace this with a random word generator
-      currentTurn: citel.sender,
-      guessedLetters: [],
-    };
-    
-    citel.reply("A new Hangman game has started. Type '.guess <letter>' to play!");
-    
-    this.hangmanGame[room.id] = room;
-  }
-);
+const words = require('../lib/words.js'); // array of words to pick from
 
-cmd(
-  {
-    on: "text"
-  },
-  async (Void, citel, text) => {
-    if (!citel.isGroup || typeof text !== 'string') return;
-    let { prefix } = require('../lib');
-    
-    this.hangmanGame = this.hangmanGame ? this.hangmanGame : {};
-    let room = Object.values(this.hangmanGame).find(
-      (room) =>
-        room.id &&
-        room.players.includes(citel.sender) &&
-        room.word &&
-        room.currentTurn === citel.sender
-    );
-    
-    if (!room) return;
-    
-    let guessCommand = text.toLowerCase().split(" ");
-    if (guessCommand[0] === ".guess" && guessCommand.length === 2) {
-      let guessedLetter = guessCommand[1];
-      if (guessedLetter.length !== 1 || !/[a-z]/.test(guessedLetter)) return;
-      
-      room.guessedLetters.push(guessedLetter);
-      let formattedWord = room.word
-        .split('')
-        .map(letter => (room.guessedLetters.includes(letter) ? letter : '_'))
-        .join(' ');
-      
-      // Check if the guessed letter is correct
-      if (room.word.includes(guessedLetter)) {
-        await Void.sendMessage(citel.chat, `Good guess! ${guessedLetter} is in the word.`);
-      } else {
-        await Void.sendMessage(citel.chat, `${guessedLetter} is not in the word. Keep trying!`);
-      }
-      
-      // Update the current turn (you might want to implement a way to alternate turns)
-      room.currentTurn = room.players.find(player => player !== room.currentTurn);
-      
-      // Send updated game state to the group
-      let gameStateMessage = `Current word: ${formattedWord}\nGuessed letters: ${room.guessedLetters.join(', ')}`;
-      await Void.sendMessage(citel.chat, gameStateMessage);
-      
-      // Check for game over conditions
-      if (!formattedWord.includes('_')) {
-        await Void.sendMessage(citel.chat, `Congratulations! You've guessed the word: ${room.word}`);
-        delete this.hangmanGame[room.id];
-      } else if (room.guessedLetters.length === 6) {
-        await Void.sendMessage(citel.chat, `Sorry, you've run out of guesses. The word was: ${room.word}`);
-        delete this.hangmanGame[room.id];
-      }
+cmd({
+  pattern: 'hangman',
+  desc: 'Start a game of hangman',
+  category: 'Games'
+}, async (Void, citel) => {
+
+  let roomId = 'hangman-' + Date.now();
+  let word = randomWord(words); 
+  let maskedWord = maskWord(word);
+  let remainingGuesses = 6;
+
+  citel.reply(`Starting hangman in room ${roomId}`)
+  citel.reply(maskedWord);
+
+  this.rooms[roomId] = {
+    id: roomId, 
+    word,
+    maskedWord,
+    remaining: remainingGuesses,
+    player: citel.sender
+  };
+
+});
+
+cmd({
+  on: 'text',
+  priority: 1
+}, async (Void, citel, text) => {
+
+  let room = Object.values(this.rooms).find(room => 
+    room.player === citel.sender && 
+    room.id.startsWith('hangman')
+  );
+
+  if (!room) return;
+
+  let guess = text.toLowerCase()[0];
+
+  if (room.word.includes(guess)) {
+    room.maskedWord = fillInLetter(room.maskedWord, room.word, guess); 
+    if (!room.maskedWord.includes('_')) {
+      citel.reply(`You guessed the word: ${room.word}`);
+      delete this.rooms[room.id];
+      return; 
     }
+  } else {
+    room.remaining--;
   }
-);
 
+  if (room.remaining === 0) {
+    citel.reply(`You lost! The word was ${room.word}`);
+    delete this.rooms[room.id];       
+    return;
+  }
+
+  citel.reply(`Guess: ${guess}, Remaining: ${room.remaining}`);
+  citel.reply(room.maskedWord);
+
+})
+
+// helper functions 
+
+function randomWord(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function maskWord(word) {
+  return word.replace(/./g, '_');
+} 
+
+function fillInLetter(masked, word, letter) {
+  let idx = word.indexOf(letter);
+  while (idx !== -1) {
+    masked = masked.substring(0,idx) + letter + masked.substring(idx+1); 
+    idx = word.indexOf(letter, idx + 1);
+  }
+  return masked;
+}

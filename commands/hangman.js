1b673dcd5f6ac59c37f556da33b5db06f0682a77
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-16 21:02:05 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -54,16 +54,31 @@ cmd(
       if (guessedLetter.length !== 1 || !/[a-z]/.test(guessedLetter)) return;
       
       room.guessedLetters.push(guessedLetter);
-      // Implement logic to check if the guessed letter is correct and update the game state
+      let formattedWord = room.word
+        .split('')
+        .map(letter => (room.guessedLetters.includes(letter) ? letter : '_'))
+        .join(' ');
+      
+      // Check if the guessed letter is correct
+      if (room.word.includes(guessedLetter)) {
+        await Void.sendMessage(citel.chat, `Good guess! ${guessedLetter} is in the word.`);
+      } else {
+        await Void.sendMessage(citel.chat, `${guessedLetter} is not in the word. Keep trying!`);
+      }
+      
+      // Update the current turn (you might want to implement a way to alternate turns)
+      room.currentTurn = room.players.find(player => player !== room.currentTurn);
       
       // Send updated game state to the group
-      let gameStateMessage = `Current word: ${formattedWord}\nGuessed letters: ${formattedGuessedLetters}`;
+      let gameStateMessage = `Current word: ${formattedWord}\nGuessed letters: ${room.guessedLetters.join(', ')}`;
       await Void.sendMessage(citel.chat, gameStateMessage);
       
-      // Check for game over conditions and update game state accordingly
-      
-      // If the game is over, remove the room from the hangmanGame object
-      if (gameIsOver) {
+      // Check for game over conditions
+      if (!formattedWord.includes('_')) {
+        await Void.sendMessage(citel.chat, `Congratulations! You've guessed the word: ${room.word}`);
+        delete this.hangmanGame[room.id];
+      } else if (room.guessedLetters.length === 6) {
+        await Void.sendMessage(citel.chat, `Sorry, you've run out of guesses. The word was: ${room.word}`);
         delete this.hangmanGame[room.id];
       }
     }

5633c6d177aa386d9b3739830a75fb36e3da4305
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-17 00:42:52 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,59 +1,41 @@
 const { cmd } = require("../lib/");
-const words = require('../lib/words.js');
+const Hangman = require('../lib/hangman');
+const words = require('../lib/words');
 
-cmd({
+const cmd = {
   pattern: 'hangman',
   desc: 'Start a game of hangman',
   category: 'Games'
-}, async (Void, citel) => {
-  const chosenWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
-  let guessedLetters = [];
-  let remainingGuesses = 6;
-  let revealedWord = '';
+};
 
-  // Initialize the revealed word with dashes
-  for (let i = 0; i < chosenWord.length; i++) {
-    revealedWord += '-';
-  }
+cmd.command = async (Void, citel) => {
+  const sender = citel.sender;
+  const reply = citel.reply;
 
-  citel.reply('Welcome to Hangman! Guess the word by typing a letter.');
+  const randomWord = getRandomWord();
+  const hangman = new Hangman(randomWord);
 
-  while (remainingGuesses > 0 && revealedWord.includes('-')) {
-    citel.reply(`\n${revealedWord}\nRemaining guesses: ${remainingGuesses}`);
+  reply('Hangman Game Started!');
+  reply('Guess the word by entering one letter at a time.');
 
-    const response = await citel.sender.nextMessage();
-    const guessedLetter = response.body.toLowerCase();
+  hangman.printWordState();
 
-    if (guessedLetters.includes(guessedLetter)) {
-      citel.reply(`You already guessed the letter "${guessedLetter}". Try again.`);
-      continue;
-    }
+  citel.on('message', (message) => {
+    if (message.sender === sender) {
+      const letter = message.content.trim();
 
-    guessedLetters.push(guessedLetter);
+      hangman.makeGuess(letter);
 
-    if (chosenWord.includes(guessedLetter)) {
-      // Update the revealed word with the correctly guessed letter
-      let tempWord = '';
-      for (let i = 0; i < chosenWord.length; i++) {
-        if (chosenWord[i] === guessedLetter) {
-          tempWord += guessedLetter;
-        } else {
-          tempWord += revealedWord[i];
-        }
+      if (hangman.maxGuesses === 0 || hangman.word.split('').every(char => hangman.guesses.has(char))) {
+        citel.removeAllListeners('message');
       }
-      revealedWord = tempWord;
-
-      citel.reply(`Good guess! The letter "${guessedLetter}" is in the word.`);
-    } else {
-      remainingGuesses--;
-
-      citel.reply(`Oops! The letter "${guessedLetter}" is not in the word. Remaining guesses: ${remainingGuesses}`);
     }
-  }
+  });
+};
 
-  if (remainingGuesses > 0) {
-    citel.reply(`Congratulations! You guessed the word "${chosenWord}" correctly.`);
-  } else {
-    citel.reply(`Game over! The word was "${chosenWord}". Better luck next time.`);
-  }
-});
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex];
+}
+
+module.exports = cmd;

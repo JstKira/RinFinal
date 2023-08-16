b7a37a27659b8256120f2393005c757f2737bbb6
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-16 22:46:38 +0300

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -1,6 +1,19 @@
 const { cmd, parseJid, getAdmin, tlang } = require("../lib/");
+const { MongoClient } = require("mongodb");
+const { sck1 } = require("../lib/database/user.js"); // Update with the correct path to the UserSchema file
 
-let rooms = {}; // Initialize rooms as an empty object
+const mongoURI = "mongodb+srv://thamerrin:Thamer12@rin.kikwxum.mongodb.net/?retryWrites=true&w=majority"; // Update with your MongoDB connection URI
+
+let db; // MongoDB database connection
+
+MongoClient.connect(mongoURI, (err, client) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+    return;
+  }
+  db = client.db("hangmanGame"); // Update with your preferred database name
+  console.log("Connected to MongoDB!");
+});
 
 //----------------------
 const words = require('../lib/words.js'); // array of words to pick from
@@ -11,10 +24,10 @@ cmd({
   category: 'Games'
 }, async (Void, citel) => {
 
-  let roomId = 'hangman-' + Date.now();
-  let word = randomWord(words); 
-  let maskedWord = maskWord(word);
-  let remainingGuesses = 6;
+  const roomId = 'hangman-' + Date.now();
+  const word = randomWord(words); 
+  const maskedWord = maskWord(word);
+  const remainingGuesses = 6;
 
   console.log(`Starting hangman in room ${roomId}`);
   console.log(`Word: ${word}`);
@@ -23,13 +36,18 @@ cmd({
   citel.reply(`Starting hangman in room ${roomId}`);
   citel.reply(maskedWord);
 
-  rooms[roomId] = {
+  const user = await sck1.findOne({ id: citel.sender }); // Find the user based on the sender ID
+
+  const room = {
     id: roomId, 
     word,
     maskedWord,
     remaining: remainingGuesses,
-    player: citel.sender
+    player: user // Store the reference to the user
   };
+
+  // Store the game room in MongoDB
+  db.collection("hangmanRooms").insertOne(room);
 
 });
 
@@ -40,14 +58,18 @@ cmd({
 
   console.log(`Received text message: ${text}`);
 
-  let room = Object.values(rooms).find(room => 
-    room.player === citel.sender && 
-    room.id.startsWith('hangman')
-  );
+  // Find the user based on the sender ID
+  const user = await sck1.findOne({ id: citel.sender });
+
+  // Find the game room in MongoDB based on the user and room ID
+  const room = await db.collection("hangmanRooms").findOne({
+    'player.id': user.id,
+    id: { $regex: /^hangman-/ }
+  });
 
   if (!room || typeof text !== 'string' || text.length === 0) return;
 
-  let guess = text.toLowerCase()[0];
+  const guess = text.toLowerCase()[0];
 
   console.log(`Guess: ${guess}`);
 
@@ -55,7 +77,8 @@ cmd({
     room.maskedWord = fillInLetter(room.maskedWord, room.word, guess); 
     if (!room.maskedWord.includes('_')) {
       citel.reply(`You guessed the word: ${room.word}`);
-      delete rooms[room.id];
+      // Delete the game room from MongoDB
+      db.collection("hangmanRooms").deleteOne({ id: room.id });
       return; 
     }
   } else {
@@ -64,7 +87,8 @@ cmd({
 
   if (room.remaining === 0) {
     citel.reply(`You lost! The word was ${room.word}`);
-    delete rooms[room.id];       
+    // Delete the game room from MongoDB
+    db.collection("hangmanRooms").deleteOne({ id: room.id });
     return;
   }
 
@@ -74,9 +98,9 @@ cmd({
   citel.reply(`Guess: ${guess}, Remaining: ${room.remaining}`);
   citel.reply(room.maskedWord);
 
-})
+});
 
-// helper functions 
+// Helper functions
 
 function randomWord(words) {
   return words[Math.floor(Math.random() * words.length)];
@@ -89,7 +113,7 @@ function maskWord(word) {
 function fillInLetter(masked, word, letter) {
   let idx = word.indexOf(letter);
   while (idx !== -1) {
-    masked = masked.substring(0,idx) + letter + masked.substring(idx+1); 
+    masked = masked.substring(0, idx) + letter + masked.substring(idx + 1); 
     idx = word.indexOf(letter, idx + 1);
   }
   return masked;

4471d5a0c7f15e868454c0a2c24abaa24f3c4e32
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-20 21:43:01 +0300

    Update Franken.js

--- Themes/Franken.js
+++ Themes/Franken.js
@@ -1,1 +1,64 @@
 
+{
+    "_JSON_": true,
+    "AUTHOR": "Thamer1t",
+    "LANGUAGE": "Arabic",
+    "LANGCODE": "ar",
+    "STFrankenstienGS": {
+    "updater": {
+    "UPDATE": "محدث اخر تحديث ",
+    "NEW_UPDATE": "ترا فيه تحديث للبوت!\n\n*Changes:*\n",
+    "UPDATED_LOCAL": " تم التحديث!"
+    },
+    "global": {
+    "botName": " 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 Frankenstien",
+    
+    "body": "Frankenstien 𝐵𝑜𝑡",
+    "greet2" : "ارحب",
+    "pic1": "https://static.wikia.nocookie.net/noblesse/images/8/8a/497_74_Frankenstein_Makes_His_Decision.png",
+    "pic2": "https://images.alphacoders.com/230/230233.jpg",
+    "pic3": "https://i.pinimg.com/originals/ed/1b/f1/ed1bf10603874b8f8449c7de5e5e158a.jpg",
+    "pic4": "https://i.pinimg.com/564x/69/67/fd/6967fd0d7fd85009ad51bfaf2c375721.jpg",
+    "pic5": "https://i.pinimg.com/564x/f5/5c/3a/f55c3ac99481d716c7be48826ddb61e9.jpg",
+    "pic6": "https://i.pinimg.com/originals/48/45/aa/4845aa038e2d983c99a0bf75bffadc37.jpg",
+    "pic7": "https://i.pinimg.com/originals/22/99/05/229905f4a2209de27ad351c212c9842c.jpg",
+    "gif1": "https://i.pinimg.com/originals/7e/4f/67/7e4f67326eb04b439003e396824f12ff.gif",
+    "gif2": "https://i.pinimg.com/originals/d0/8e/0b/d08e0b7c72d70625cca1a45e83b3266a.gif",
+    "gif3": "https://i.pinimg.com/originals/b1/ec/a9/b1eca9823adeff6b676f2056d33a7e86.gif",
+    "gif4": "https://i.pinimg.com/originals/7e/4f/67/7e4f67326eb04b439003e396824f12ff.gif",
+    "gif5": "https://i.pinimg.com/originals/b1/ec/a9/b1eca9823adeff6b676f2056d33a7e86.gif",
+    "gif6": "https://i.pinimg.com/originals/b1/ec/a9/b1eca9823adeff6b676f2056d33a7e86.gif",
+    "gif7": "https://i.pinimg.com/originals/d0/8e/0b/d08e0b7c72d70625cca1a45e83b3266a.gif",
+    "greet" : "ياهلا ومرحبا",
+    "dotbody" : "وش يفيد البوح اذا المقصود لوح",
+    "promote": "عالبركة ترقيت",
+    "pmallow": " مشغول شويتين، اترك رسالتك  برد عليك بعد شوي",
+    "pmblock": "❌ Blocked*\n\n تم حظرك عشانك تسوي سبام، اعقل!.\n\n _ 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 Frankenstien",
+    "pmwarn": "✋مرحبا* \nPlease لاتسوي سبام ولا ببلكك عندك ${checkPermit.times} انذار(ات).\n\n * 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 Frankenstien*",
+    "permited": "هلا والله ${tname} *\n\n تم السماح لك الان بالرسائل الخاصة.\n\n 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 Frankenstien",
+    "revoked" : "مرحبا ${tname} Sir\n\n ماعليش ماتقدر تستخدم البوت خاص الحين .\n\n 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 Frankenstien",
+    "demote": "انشال اشرافك",
+    "antlink": "اوبا فيه رابط!\n*نشوفك على خير*",
+    "session": "\n*This is your session.sam.json,keep it safe and store it properly*\n_Now Upload This json file in Your Forked Repo_\n Connect your repo to Heroku and Deploy Branch and Bot will run for Always\n\n*𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐆𝐨𝐤𝐮*",
+    "mention": " لازم تسوي منشن",
+    "title": "Frankenstien",
+    "footer": "Frankenstien ",
+    "by": "Made by: Frankenstien",
+    "chat_desc": "Options Avilable* : 1. mute\n2. unmute\n3. archive\n4. unarchive\n5. read\n6. unread\n7. delete",
+    "success": "تم ✓",
+    "dot": "ارحب, اذا تبي مساعدة اكتب -help",
+    "couple_male": "ا",
+    "couple_female": "ا",
+    "admin": "الامر خاص بالمشرفين⚠️!",
+    "botAdmin": "عطني اشراف وازهلها .❗",
+    "owner": "هالأمر خاص بغومونريونغ ⚠️",
+    "group": "خاص بالقروبات❗",
+    "private": "خاص بغومونريونغ ⚠️",
+    "bot": "خاص بالبوت⚠️",
+    "wait": "شويات بس.....✅"
+    },
+    "error": {
+    "text": "صبر فيه خطأ"
+    }
+    }
+    }

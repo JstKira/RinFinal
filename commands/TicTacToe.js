6c1a3c6704722dc079a0bcc7faa9a32902e4b793
Thamer Alshammari <145974430+Rin1t@users.noreply.github.com>
2024-03-19 05:37:59 +0300

    Update TicTacToe.js

--- commands/TicTacToe.js
+++ commands/TicTacToe.js
@@ -224,37 +224,6 @@ ${
 
 //===============================================
 
-cmd(
-  {
-    pattern: "حوم",
-    desc: " لعبة حجرة ورقة مقص",
-    category: "العاب",
-  },
-  async (Void, citel, text) => {
-    if (!citel.isGroup) return citel.reply(tlang().group);
-    let { prefix } = require('../lib')
-    let choices = ["حجرة", "ورقة", "مقص"];
-    let botIndex = Math.floor(Math.random() * choices.length);
-    let botChoice = choices[botIndex];
-    let userChoice = text.toLowerCase();
-    if (!choices.includes(userChoice)) {
-      return citel.reply("اختر بين: حجرة، ورقة، مقص.");
-    }
-    if (botChoice === userChoice) {
-      return citel.reply("تعادل!");
-    }
-    if (
-      (botChoice === "حجرة" && userChoice === "مقص") ||
-      (botChoice === "ورقة" && userChoice === "حجرة") ||
-      (botChoice === "مقص" && userChoice === "ورقة")
-    )  {
-      return citel.reply(`أنا اخترت ${botChoice}، فزت عليك!`);
-    } else {
-      return citel.reply(`أنا اخترت ${botChoice}، لقد فزت!`);
-    }
-  }
-);
-
 //===============================================================
 
 cmd({ pattern: "صداقة" , category: "ترفيه" }, async(Void, citel, text) => {

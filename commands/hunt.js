49f6f70a23ae8f4597806c332d64ba8bc01db2b5
Thamer Alshammari <145974430+Rin1t@users.noreply.github.com>
2024-03-24 15:38:32 +0300

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const { cmd, prefix } = require('../lib');
 const { RandomXP } = require('../lib/database/xp');
 const { sck1 } = require('../lib/database/user');
@@ -141,3 +141,4 @@ cmd(
     }
   }
 );
+*/

a0dc15b250d496a9bea0e423a792056360f7e039
Thamer Alshammari <145974430+Rin1t@users.noreply.github.com>
2024-04-05 08:02:54 +0300

    Update order.js

--- commands/order.js
+++ commands/order.js
@@ -50,7 +50,7 @@ cmd(
   async (Void, citel, text) => {
     if (!games[citel.sender]) return; // No active game for the user
     const game = games[citel.sender];
-    if (citel.quoted.sender !== '966508206360@s.whatsapp.net' || citel.quoted.id !== game.questionMessageId) {
+    if (citel.quoted.id !== game.questionMessageId) {
       return; // Ignore if the user's reply is not to the correct question message
     }
     const guess = citel.text;

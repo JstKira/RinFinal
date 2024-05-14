16bd2b0051eeea50195bbe2d2309ef30d235636f
Thamer Alshammari <145974430+Rin1t@users.noreply.github.com>
2024-05-14 03:19:27 +0300

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -17,50 +17,54 @@ cmd(
     category: "العاب",
   },
   async (Void, citel, text) => {
-    if (!games[citel.sender]) {
-      const questionData = qaData[Math.floor(Math.random() * qaData.length)];
-      const question = questionData.question;
-      const response = questionData.response;
+    try {
+      if (!games[citel.sender]) {
+        const questionData = qaData[Math.floor(Math.random() * qaData.length)];
+        const question = questionData.question;
+        const response = questionData.response;
 
-      // Fetch the thumbnail buffer (replace 'nicepic' with the URL of the thumbnail image)
-      const thumbnailBuffer = await getBuffer(nicepic);
+        // Fetch the thumbnail buffer (replace 'nicepic' with the URL of the thumbnail image)
+        const thumbnailBuffer = await getBuffer(nicepic);
 
-      let mediaData = {
-        text: `❓ ${question} ❓`,
-        mimetype: 'text/plain',
-        ptt: false,
-        headerType: 1,
-        contextInfo: {
-          forwardingScore: 999,
-          isForwarded: false,
-          externalAdReply: {
-            title: nicetitle, // Replace with your title
-            body: nicebody, // Replace with your body text
-            renderLargerThumbnail: true,
-            thumbnail: thumbnailBuffer,
-            mediaUrl: '',
-            mediaType: 1,
-            showAdAttribution: true
+        let mediaData = {
+          text: `❓ ${question} ❓`,
+          mimetype: 'text/plain',
+          ptt: false,
+          headerType: 1,
+          contextInfo: {
+            forwardingScore: 999,
+            isForwarded: false,
+            externalAdReply: {
+              title: nicetitle, // Replace with your title
+              body: nicebody, // Replace with your body text
+              renderLargerThumbnail: true,
+              thumbnail: thumbnailBuffer,
+              mediaUrl: '',
+              mediaType: 1,
+              showAdAttribution: true
+            }
           }
-        }
-      };
+        };
 
-     await Void.sendMessage(citel.chat, mediaData, { quoted: citel });
+        await Void.sendMessage(citel.chat, mediaData, { quoted: citel });
 
-      games[citel.sender] = {
-        question: question,
-        response: response
-      };
+        games[citel.sender] = {
+          question: question,
+          response: response
+        };
 
-      // Set a timer for 60 seconds
-      setTimeout(() => {
-        if (games[citel.sender]) {
-          delete games[citel.sender]; // Delete the game
-          citel.reply("*انتهى الوقت*\n\n`الجواب:`" + response);
-        }
-      }, 60000); // 120 seconds in milliseconds
-    } else {
-      citel.reply("لديك لعبة نشطة بالفعل!");
+        // Set a timer for 60 seconds
+        setTimeout(() => {
+          if (games[citel.sender]) {
+            delete games[citel.sender]; // Delete the game
+            citel.reply("*انتهى الوقت*\n\n`الجواب:`" + response);
+          }
+        }, 60000); // 120 seconds in milliseconds
+      } else {
+        citel.reply("لديك لعبة نشطة بالفعل!");
+      }
+    } catch (error) {
+      console.error("Error in 'سؤال' command:", error);
     }
   }
 );
@@ -70,22 +74,26 @@ cmd(
     on: "text"
   },
   async (Void, citel, text) => {
-    if (!games[citel.sender]) return; // No active game for the user
-    const game = games[citel.sender];
+    try {
+      if (!games[citel.sender]) return; // No active game for the user
+      const game = games[citel.sender];
 
-    // Check if the message is a reply and the original message's sender is not the bot itself
-    if (citel.quoted.sender !== '966508206360@s.whatsapp.net') {
-      return; // If there's no quoted message or if the sender doesn't match, do nothing
-    }
-    
-    const guess = citel.text;
-    const correctAnswer = game.response;
+      // Check if the message is a reply and the original message's sender is not the bot itself
+      if (citel.quoted.sender !== '966508206360@s.whatsapp.net') {
+        return; // If there's no quoted message or if the sender doesn't match, do nothing
+      }
+      
+      const guess = citel.text;
+      const correctAnswer = game.response;
 
-    if (guess === correctAnswer) {
-      citel.reply(`🎉 *صححح عليك!*`);
-      delete games[citel.sender];
-    } else {
-      citel.reply(`❌ *خطأ*! حاول مرة ثانية`);
+      if (guess === correctAnswer) {
+        citel.reply(`🎉 *صححح عليك!*`);
+        delete games[citel.sender];
+      } else {
+        citel.reply(`❌ *خطأ*! حاول مرة ثانية`);
+      }
+    } catch (error) {
+      console.error("Error in text event:", error);
     }
   }
 );
